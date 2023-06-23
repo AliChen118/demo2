1c64cd0bb6ad4b10a7e56df970f624ea1a60a08f
Ali <1185462216@qq.com>
2023-06-23 10:17:07 +0800

    delete f1()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        f1();
-    }
-
-    public static void f1() {
-        System.out.println("f1");
     }
 }
